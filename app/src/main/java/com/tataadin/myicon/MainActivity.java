7ccde925672961d39c7a15e34f2611f64c8c195d
Tata Adi Nugroho <tataa2237@gmail.com>
2019-09-29 01:55:59 +0700

    initial

---
+++ app/src/main/java/com/tataadin/myicon/MainActivity.java
@@ -0,0 +1,12 @@
+package com.tataadin.myicon;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
